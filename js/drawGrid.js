fa622c0a225970807aba39543fd3d407b6a33f22
Lorin <Lorin.Petersen@gmail.com>
2012-02-03 01:49:09 -0500

    Added image directory and modified draw code to accept png

--- js/drawGrid.js
+++ js/drawGrid.js
@@ -27,10 +27,15 @@ window.onload = function () {
 
 	var canvas = document.getElementById('myCanvas');
 	var c = canvas.getContext('2d');
-
+	
+	//Temp loading in image for toolbox placeholder
+	this.tb1 = new Image();
+	this.tb1.src = "image/orange.png";
+	
 	canvas.addEventListener('click', handleClick, false);
 
-	draw();
+	drawGrid();
+	
 
 	function handleClick(e) {
 		// When a click is detected, translate the mouse coordinates to pixel coordinates
@@ -43,11 +48,9 @@ window.onload = function () {
 		tileMap[row][column] = 1;
 	}
 
-	function draw() {
-
+	function drawGrid() {
 		c.fillStyle = '#FFFFFF';
-		c.fillRect (0, 0, canvas.width, canvas.height);
-		//c.fillStyle = '#000000';
+		c.fillRect (0, 0, canvas.width, canvas.height);		
 
 		var startRow = Math.floor(scroll.x / tile.width);
 		var startCol = Math.floor(scroll.y / tile.height);
@@ -66,16 +69,28 @@ window.onload = function () {
 				tilePositionY -= scroll.y;
 				
 				if (tileMap[row] != null && tileMap[row][col] != null) {
-					c.fillStyle = '#CC0000';
-					c.fillRect(tilePositionX, tilePositionY, tile.width, tile.height);
-					c.fillStyle = '#000000';
-					c.strokeRect(tilePositionX, tilePositionY, tile.width, tile.height);
+				
+					//We're drawing the image on click, instead of changing the fill on the squares
+					
+					//c.fillStyle = '#CC0000';
+					//c.fillRect(tilePositionX, tilePositionY, tile.width, tile.height);
+					//c.fillStyle = '#000000';
+					//c.strokeRect(tilePositionX, tilePositionY, tile.width, tile.height);
+					c.drawImage(this.tb1, tilePositionX, tilePositionY, this.tb1.width, this.tb1.height);
+					
 				} else {
 					c.strokeRect(tilePositionX, tilePositionY, tile.width, tile.height);
 				}
 			}
-		}
+		}	
+		drawToolBox(); //Add the toolbox to the bottom of the grid
 		
-		setTimeout(draw, 1);
+		setTimeout(drawGrid, 1);
+	}
+	
+	function drawToolBox() {
+		c.fillStyle = '#FFFFFF';
+		c.fillRect (0, grid.height * tile.height, grid.width * tile.width, tile.height);
+		c.strokeRect(0, grid.height * tile.height, grid.width * tile.width, tile.height);
 	}
 }
